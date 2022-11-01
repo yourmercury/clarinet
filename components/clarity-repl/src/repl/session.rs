8548484ba2f20533a0b7c935d1866ee658175a32
teen0809 <throwy.teen@gmail.com>
2022-11-01 14:59:54 -0400

    fix get blockchain height

--- components/clarity-repl/src/repl/session.rs
+++ components/clarity-repl/src/repl/session.rs
@@ -828,7 +828,7 @@ impl Session {
 
     fn get_block_height(&mut self, output: &mut Vec<String>) {
         let height = self.interpreter.get_block_height();
-        output.push("{}", height);
+        output.push(format!("{}", height));
     }
 
     fn get_account_name(&self, address: &String) -> Option<&String> {
